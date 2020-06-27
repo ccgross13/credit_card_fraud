a9a8e28ef28e05ab178efbf02e9717d083995afe
ccgross13 <63924715+ccgross13@users.noreply.github.com>
2020-06-27 13:01:43 -0500

    Add files via upload

---
+++ Data Schema.sql
@@ -0,0 +1,61 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/HtXcKX
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "credit_card" (
+    "card" BIGINT   NOT NULL,
+    "id_card_holder" INT   NOT NULL,
+    CONSTRAINT "pk_credit_card" PRIMARY KEY (
+        "card"
+     )
+);
+
+CREATE TABLE "card_holder" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_card_holder" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "merchant_category" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_merchant_category" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "merchant" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(50)   NOT NULL,
+    "id_merchant_category" INT   NOT NULL,
+    CONSTRAINT "pk_merchant" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "transaction" (
+    "id" INT   NOT NULL,
+    "date" TIMESTAMP   NOT NULL,
+    "amount" FLOAT   NOT NULL,
+    "card" BIGINT   NOT NULL,
+    "id_merchant" INT   NOT NULL,
+    CONSTRAINT "pk_transaction" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "credit_card" ADD CONSTRAINT "fk_credit_card_card" FOREIGN KEY("card")
+REFERENCES "transaction" ("card");
+
+ALTER TABLE "credit_card" ADD CONSTRAINT "fk_credit_card_id_card_holder" FOREIGN KEY("id_card_holder")
+REFERENCES "card_holder" ("id");
+
+ALTER TABLE "merchant_category" ADD CONSTRAINT "fk_merchant_category_id" FOREIGN KEY("id")
+REFERENCES "merchant" ("id");
+
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_id_merchant" FOREIGN KEY("id_merchant")
+REFERENCES "merchant" ("id");
+
